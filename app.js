43ceb3a80ae722680858bcc8449187d492677eab
Nitish Kumar <101493999+pnnitish9@users.noreply.github.com>
2025-11-19 21:47:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Global Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'https://expense-tracker-ten-rosy-30.vercel.app',
   credentials: true,
 }));
 app.use(express.json());
